90aa2128f9c4eba0d2c0ff050e89dc06c004d487
Swanith <swanitharvety@gmail.com>
2022-04-25 19:37:27 +0530

    Made so that the program stops when the user's mouse cursor is moved to the top-left corner of their screen.

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import random
 import time
+import sys
 from charectors import char
+import pyautogui
 
 print("Starting...")
 time.sleep(1)
 
 print("Charectors: \n", char)
+print()
 
 while True:
     randomized_symbol = random.choice(char)
     time.sleep(0.1)
     print(randomized_symbol)
+
+    pos = pyautogui.position()
+    #print(pos)
+    if pos == ("Point(x=0, y=0)"):
+        sys.exit()
+    else:
+        pass
