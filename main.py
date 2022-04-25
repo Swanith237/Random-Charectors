f2c2d67b54c04a0d466ca2ae11ebf4050c02fb7b
Swanith <swanitharvety@gmail.com>
2022-04-25 19:52:09 +0530

    Added Credits

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ def program():
     import random
     import time
     import sys
-    from charectors import char
     import pyautogui
+    import colorama
+    from colorama import Style
+    from charectors import char
+
+    colorama.init()
 
     print("This program is going to be continuously printing random charectors into the terminal.")
-
     def conf():
         confirm = input("Are you sure you want to continue? (y/n): ")
 
@@ -32,6 +35,12 @@ def program():
         elif confirm == "n":
             print("Okay, Bye!")
             time.sleep(2)
+
+            print(f"{Style.BRIGHT}Made by: Swanith")
+            print(f"{Style.BRIGHT}Discord: FightKnight#6129")
+            print(f"{Style.BRIGHT}Github: Swanith237")
+
+            time.sleep(10)
             sys.exit()
 
     conf()
