7109e5eebc5aceba24371c225138ac7c44c60b0c
Swanith <swanitharvety@gmail.com>
2022-04-25 19:08:58 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import random
+import time
 
+print("Starting...")
+time.sleep(1)
+
+char =["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "-", "_", "+", "=", "\\", "/", "|", ":", ";", "'", "\"", "?", ".", ",", "()", "[]", "{}", "<>", " "]
+print("Charectors: \n", char)
+
+while True:
+    randomized_symbol = random.choice(char)
+    time.sleep(0.1)
+    print(randomized_symbol)
